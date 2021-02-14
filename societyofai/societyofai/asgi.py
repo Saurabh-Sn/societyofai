ee2ebcdee7432ff7bd8488d23885a09fe35b5bb1
saurabh <saurabhsingh@zapbuild.com>
2021-02-14 17:06:34 +0530

    created project

---
+++ societyofai/societyofai/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for societyofai project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'societyofai.settings')
+
+application = get_asgi_application()
