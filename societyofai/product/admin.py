c9a0860827e49c5a1ad8ec3a489fdcb5ac07a1d8
saurabh <saurabhnegi73@gmail.com>
2021-02-14 21:37:58 +0530

    add order in admin

--- societyofai/product/admin.py
+++ societyofai/product/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Order
 # Register your models here.
 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                    'order_date',
+                    'status'
+
+                    ]
+    list_display_links = [
+        'user',
+
+    ]
+    list_filter = ['status',
+                  ]
+    search_fields = [
+        'user__email',
+
+    ]
+    readonly_fields = ('product',)
+    
+    def order_date(self, obj):
+        if obj:
+            return obj.created_at.date()
+        return '-'
+
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Product)
 admin.site.register(Category)
